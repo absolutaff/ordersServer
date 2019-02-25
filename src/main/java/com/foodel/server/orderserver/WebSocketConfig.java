c8d4d7969d916fd437a1820595c2da50d793a073
foodel27 <foodel27@gmail.com>
2019-02-25 17:21:04 +0200

    CrossOrigins in registry

--- src/main/java/com/foodel/server/orderserver/WebSocketConfig.java
+++ src/main/java/com/foodel/server/orderserver/WebSocketConfig.java
@@ -17,6 +17,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
-		registry.addEndpoint("/foodel-websocket").withSockJS();
+		registry.addEndpoint("/foodel-websocket").setAllowedOrigins("*").withSockJS();
 	}
 }
